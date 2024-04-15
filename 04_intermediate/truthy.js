aded10d9509af4395febbfe563b911f89fa0e8fd
Deb Dutta <113854581+debdutta14@users.noreply.github.com>
2024-04-15 07:54:21 +0000

    inter-1

---
+++ 04_intermediate/truthy.js
@@ -0,0 +1,17 @@
+// how to check a Array is empty or not
+const user=[]
+// if(user.length === 0){
+//     console.log("Array empty");
+// }
+
+// //check object empty or not
+// const buy={}
+// if(Object.keys(buy).length === 0){
+//     console.log("Empty Object");
+// }
+
+//Nullish Coalescing Operator (??)
+let val1;
+val1= null ?? 15;
+
+console.log(val1);
